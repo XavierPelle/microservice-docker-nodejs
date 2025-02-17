f9dd4aaeecb938287c8b9372f7a3df51c4bcf185
FOTSO TEMEU Samuel Daryl <temmodaryl317@gmail.com>
2025-02-18 00:07:19 +0100

    Page Acceuil et design responsive fait

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cart', component: CartComponent, canActivate: [AuthGuard]},
