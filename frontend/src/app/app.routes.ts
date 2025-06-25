0629c556871aa10d8682be7ea1e636e792a73ecb
Issa KOUYATE <issakouyate1012@gmail.com>
2025-06-25 16:38:53 +0200

    Ajout des dashboards front (admin, user, vendor) + configs tailwind/postcss

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { AuthGuard } from './services/auth-guard.service';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { VendorDashboardComponent } from './vendor-dashboard/vendor-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,4 +17,7 @@ export const routes: Routes = [
     { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
     { path: 'product', component: ProductComponent},
     { path: 'product/:id', component: ProductDetailComponent},
+    { path: 'user-dashboard', component: UserDashboardComponent},
+    { path: 'vendor-dashboard', component: VendorDashboardComponent},
+    { path: 'admin-dashboard', component: AdminDashboardComponent},
 ];
