8fc12c4c3c7576d1f37e1827b64d193878d9f88e
winner <kodjowinnerisac@gmail.com>
2025-06-26 13:37:15 +0200

    Update de la gestion de produit

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Cart } from '../models/cart';
 import { AuthentificationService } from '../services/authentification.service';
 import { RequestBuilderService } from '../services/request-builder.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -15,7 +16,7 @@ export class CartComponent implements OnInit {
 
   cart: Cart[] = [];
 
-  constructor(private authService: AuthentificationService, private requestBuilderService: RequestBuilderService) {}
+  constructor(private authService: AuthentificationService, private requestBuilderService: RequestBuilderService, private router: Router) {}
 
   ngOnInit(): void {
     const userInfo = this.authService.getUserInfo();
@@ -45,13 +46,15 @@ export class CartComponent implements OnInit {
 
   buy(): void {
     const userInfo = this.authService.getUserInfo();
-    this.requestBuilderService.execute('post', `/transaction-history/create/${userInfo.user_id}`).subscribe({
+    /*this.requestBuilderService.execute('post', `/transaction-history/create/${userInfo.user_id}`).subscribe({
       next: data => {
         this.cart = data;
       },
       error: () => {
         console.error('Erreur lors de l achat');
       },
-    });
+    });*/
+    const total = this.calculateTotal();
+    this.router.navigate(['/payment'], { state: { amount: total } });
   }
 }
